e5475f0279840df5dc7882c5648aacf06862dd80
Lucas Nørgård <lucasnrgaard@gmail.com>
2024-01-14 07:41:22 +0100

    test: correct test based on changes to fixtures

--- tests/fixtures/file.ts
+++ tests/fixtures/file.ts
@@ -1,4 +1,4 @@
 import { join } from "node:path";
 import { what } from "./dir1/file-b";
 
-console.log(join("a", "b", what));
+console.warn(join("a", "b", what));
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,9 +1,9 @@
 import { build } from "esbuild";
-import { describe, expect, test } from "vitest";
+import { describe, expect, it } from "vitest";
 
 import ImportTransformPlugin from "../src";
 
-test("transform without platform set", async () => {
+it("transform without platform set", async () => {
   const result = await build({
     entryPoints: ["./tests/fixtures/file.ts"],
     format: "esm",
@@ -11,14 +11,14 @@ test("transform without platform set", async () => {
     write: false,
     plugins: [
       ImportTransformPlugin({
-        "node:path": "path-browserify"
-      })
+        "node:path": "path-browserify",
+      }),
     ],
-    outfile: "./tests/fixtures/out/file.js"
+    outfile: "./tests/fixtures/out/file.js",
   });
   expect({
     errors: result.errors,
-    warnings: result.warnings
+    warnings: result.warnings,
   }).toEqual({ errors: [], warnings: [] });
   expect(result.outputFiles[0].text)
     .toBe(`// tests/fixtures/node_modules/path-browserify/index.js
@@ -30,7 +30,7 @@ function join(...args) {
 var what = join("a", "b");
 
 // tests/fixtures/file.ts
-console.log(join("a", "b", what));
+console.warn(join("a", "b", what));
 `);
 });
 
@@ -38,23 +38,23 @@ describe("transform `node:path` to `path-browserify`", () => {
   const transformPlugin = ImportTransformPlugin({
     "node:path": {
       platform: "browser",
-      to: "path-browserify"
-    }
+      to: "path-browserify",
+    },
   });
 
-  test("transform when platform is set to `browser`", async () => {
+  it("transform when platform is set to `browser`", async () => {
     const result = await build({
       entryPoints: ["./tests/fixtures/file.ts"],
       bundle: true,
       platform: "browser",
       write: false,
       plugins: [transformPlugin],
-      outfile: "./tests/fixtures/out/file2.js"
+      outfile: "./tests/fixtures/out/file2.js",
     });
 
     expect({
       errors: result.errors,
-      warnings: result.warnings
+      warnings: result.warnings,
     }).toEqual({ errors: [], warnings: [] });
     expect(result.outputFiles[0].text).toBe(`"use strict";
 (() => {
@@ -67,24 +67,24 @@ describe("transform `node:path` to `path-browserify`", () => {
   var what = join("a", "b");
 
   // tests/fixtures/file.ts
-  console.log(join("a", "b", what));
+  console.warn(join("a", "b", what));
 })();
 `);
   });
 
-  test("should not transform", async () => {
+  it("should not transform", async () => {
     const result = await build({
       entryPoints: ["./tests/fixtures/file.ts"],
       bundle: true,
       platform: "node",
       write: false,
       plugins: [transformPlugin],
-      outfile: "./tests/fixtures/out/file2.js"
+      outfile: "./tests/fixtures/out/file2.js",
     });
 
     expect({
       errors: result.errors,
-      warnings: result.warnings
+      warnings: result.warnings,
     }).toEqual({ errors: [], warnings: [] });
 
     expect(result.outputFiles[0].text).toBe(`"use strict";
@@ -97,12 +97,12 @@ var import_node_path = require("node:path");
 var what = (0, import_node_path.join)("a", "b");
 
 // tests/fixtures/file.ts
-console.log((0, import_node_path2.join)("a", "b", what));
+console.warn((0, import_node_path2.join)("a", "b", what));
 `);
   });
 });
 
-test("transform `./locate` to code", async () => {
+it("transform `./locate` to code", async () => {
   const result = await build({
     entryPoints: ["./tests/fixtures/file.ts"],
     format: "esm",
@@ -113,15 +113,15 @@ test("transform `./locate` to code", async () => {
         "node:path": {
           text: `export function join(...args) {
             return args.join("/");
-          }`
-        }
-      })
+          }`,
+        },
+      }),
     ],
-    outfile: "./tests/fixtures/out/file.js"
+    outfile: "./tests/fixtures/out/file.js",
   });
   expect({
     errors: result.errors,
-    warnings: result.warnings
+    warnings: result.warnings,
   }).toEqual({ errors: [], warnings: [] });
   expect(result.outputFiles[0].text).toBe(`// import-transform:node:path
 function join(...args) {
@@ -132,6 +132,6 @@ function join(...args) {
 var what = join("a", "b");
 
 // tests/fixtures/file.ts
-console.log(join("a", "b", what));
+console.warn(join("a", "b", what));
 `);
 });
